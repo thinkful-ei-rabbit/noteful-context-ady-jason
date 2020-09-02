d9045166f66023ba3fd31bf22a8bae5547a81c8b
Jason Stankevich <jason.stankevich@gmail.com>
2020-09-02 13:50:54 -0400

    NoteListMain and NoteListNav comsumer added

--- src/NoteListNav/NoteListNav.js
+++ src/NoteListNav/NoteListNav.js
@@ -2,40 +2,45 @@ import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import CircleButton from '../CircleButton/CircleButton'
+import NotePageMainContext from '../NotePageMainContext'
 import { countNotesForFolder } from '../notes-helpers'
 import './NoteListNav.css'
 
-export default function NoteListNav(props) {
+export default function NoteListNav() {
   return (
-    <div className='NoteListNav'>
-      <ul className='NoteListNav__list'>
-        {props.folders.map(folder =>
-          <li key={folder.id}>
-            <NavLink
-              className='NoteListNav__folder-link'
-              to={`/folder/${folder.id}`}
+    <NotePageMainContext.Consumer>
+      {({ notes, folders }) =>
+        <div className='NoteListNav'>
+          <ul className='NoteListNav__list'>
+            {folders.map(folder =>
+              <li key={folder.id}>
+                <NavLink
+                  className='NoteListNav__folder-link'
+                  to={`/folder/${folder.id}`}
+                >
+                  <span className='NoteListNav__num-notes'>
+                    {countNotesForFolder(notes, folder.id)}
+                  </span>
+                  {folder.name}
+                </NavLink>
+              </li>
+            )}
+          </ul>
+          <div className='NoteListNav__button-wrapper'>
+            <CircleButton
+              tag={Link}
+              to='/add-folder'
+              type='button'
+              className='NoteListNav__add-folder-button'
             >
-              <span className='NoteListNav__num-notes'>
-                {countNotesForFolder(props.notes, folder.id)}
-              </span>
-              {folder.name}
-            </NavLink>
-          </li>
-        )}
-      </ul>
-      <div className='NoteListNav__button-wrapper'>
-        <CircleButton
-          tag={Link}
-          to='/add-folder'
-          type='button'
-          className='NoteListNav__add-folder-button'
-        >
-          <FontAwesomeIcon icon='plus' />
-          <br />
-          Folder
-        </CircleButton>
-      </div>
-    </div>
+              <FontAwesomeIcon icon='plus' />
+              <br />
+              Folder
+            </CircleButton>
+          </div>
+        </div>
+      }
+    </NotePageMainContext.Consumer>
   )
 }
 
